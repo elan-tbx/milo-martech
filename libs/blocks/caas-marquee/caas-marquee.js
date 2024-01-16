7c6de490bf53b5c8a92fab462d2cc528bea3156f
Carlos Miqueo <cmiqueo@adobe.com>
2024-01-16 15:35:15 -0800

    latest updates

--- libs/blocks/caas-marquee/caas-marquee.js
+++ libs/blocks/caas-marquee/caas-marquee.js
@@ -16,22 +16,32 @@ function getMetadata(el) {
 
 export default function init(el) {
   const metadata = getMetadata(el);
+  // const marqueeSection = createTag('div', {
+  //   class: 'caas-marquee-section',
+  //   style: `background-image: url(${metadata.image});
+  //           color: ${metadata.textcolor || '#fff'};
+  //       `,
+  // });
 
-  const preview = createTag('div', {
-    class: 'caas-marquee-preview',
-    style: `background-image: url(${metadata.image});
-            color: ${metadata.textcolor || '#fff'};
-        `,
-  });
+  const image = createTag('img', { class: 'background', src: metadata.image });
+  const imageP = createTag('p', null, image);
+  const background = createTag('div', { class: 'background' }, imageP);
 
-  const title = createTag('h1', { class: 'caas-marquee-title' }, metadata.title);
-  const detail = createTag('div', { class: 'caas-marquee-detail' }, metadata.detail);
-  const description = createTag('div', { class: 'caas-marquee-description' }, metadata.description);
-  const cta = createTag('a', { class: 'cta-link', href: metadata.ctaurl }, metadata.ctatext);
-  preview.append(detail, title, description, cta);
+  const title = createTag('h1', { class: 'heading' }, metadata.title);
+  const description = createTag('p', { class: 'text' }, metadata.description);
+  const foreground = createTag('div', { class: 'foreground' }, title, description);
 
-  const section = document.querySelector('.section');
-  section.prepend(preview);
+  const section = createTag('div', { class: 'caas-marquee-section' });
+  section.append(background, foreground);
+
+  // const title = createTag('h1', { class: 'caas-marquee-title' }, metadata.title);
+  // const detail = createTag('div', { class: 'caas-marquee-detail' }, metadata.detail);
+  // const description = createTag('div', { class: 'caas-marquee-description' }, metadata.description);
+  // const cta = createTag('a', { class: 'cta-link', href: metadata.ctaurl }, metadata.ctatext);
+  // section.append(detail, title, description, cta);
+
+  const pageSection = document.querySelector('.section');
+  pageSection.prepend(section);
 
   // degugging
   console.log(metadata); // eslint-disable-line no-console
